94f13db040b4311e76d90207cc5dfb988ec37bd7
Ivan Shafran <shafran.fun@gmail.com>
2019-06-22 15:50:59 +0300

    Add mock context and test for exception

---
+++ sharedpreferencesmock/src/main/java/com/github/ivanshafran/sharedpreferencesmock/SPMockContext.java
@@ -0,0 +1,687 @@
+package com.github.ivanshafran.sharedpreferencesmock;
+
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.ComponentCallbacks;
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentSender;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.UserHandle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.Display;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.concurrent.Executor;
+
+@SuppressLint("WrongConstant")
+final class SPMockContext extends Context {
+
+    @Override
+    public AssetManager getAssets() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Resources getResources() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public PackageManager getPackageManager() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ContentResolver getContentResolver() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Looper getMainLooper() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Context getApplicationContext() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setTheme(final int resid) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Resources.Theme getTheme() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ClassLoader getClassLoader() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getPackageName() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ApplicationInfo getApplicationInfo() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getPackageResourcePath() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getPackageCodePath() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public SharedPreferences getSharedPreferences(final String name, final int mode) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean moveSharedPreferencesFrom(final Context sourceContext, final String name) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean deleteSharedPreferences(final String name) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public FileInputStream openFileInput(final String name) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public FileOutputStream openFileOutput(final String name, final int mode) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean deleteFile(final String name) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public File getFileStreamPath(final String name) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public File getDataDir() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public File getFilesDir() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public File getNoBackupFilesDir() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Nullable
+    @Override
+    public File getExternalFilesDir(@Nullable final String type) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public File[] getExternalFilesDirs(final String type) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public File getObbDir() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public File[] getObbDirs() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public File getCacheDir() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public File getCodeCacheDir() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Nullable
+    @Override
+    public File getExternalCacheDir() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public File[] getExternalCacheDirs() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public File[] getExternalMediaDirs() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String[] fileList() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public File getDir(final String name, final int mode) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public SQLiteDatabase openOrCreateDatabase(
+            final String name,
+            final int mode,
+            final SQLiteDatabase.CursorFactory factory
+    ) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public SQLiteDatabase openOrCreateDatabase(
+            final String name,
+            final int mode,
+            final SQLiteDatabase.CursorFactory factory,
+            @Nullable final DatabaseErrorHandler errorHandler
+    ) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean moveDatabaseFrom(final Context sourceContext, final String name) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean deleteDatabase(final String name) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public File getDatabasePath(final String name) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String[] databaseList() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Drawable getWallpaper() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Drawable peekWallpaper() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getWallpaperDesiredMinimumWidth() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getWallpaperDesiredMinimumHeight() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setWallpaper(final Bitmap bitmap) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setWallpaper(final InputStream data) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void clearWallpaper() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void startActivity(final Intent intent) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void startActivity(final Intent intent, @Nullable final Bundle options) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void startActivities(final Intent[] intents) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void startActivities(final Intent[] intents, final Bundle options) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void startIntentSender(
+            final IntentSender intent,
+            @Nullable final Intent fillInIntent,
+            final int flagsMask,
+            final int flagsValues,
+            final int extraFlags
+    ) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void startIntentSender(
+            final IntentSender intent,
+            @Nullable final Intent fillInIntent,
+            final int flagsMask,
+            final int flagsValues,
+            final int extraFlags,
+            @Nullable final Bundle options
+    ) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void sendBroadcast(final Intent intent) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void sendBroadcast(final Intent intent, @Nullable final String receiverPermission) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void sendOrderedBroadcast(final Intent intent, @Nullable final String receiverPermission) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void sendOrderedBroadcast(
+            @NonNull final Intent intent,
+            @Nullable final String receiverPermission,
+            @Nullable final BroadcastReceiver resultReceiver,
+            @Nullable final Handler scheduler,
+            final int initialCode,
+            @Nullable final String initialData,
+            @Nullable final Bundle initialExtras
+    ) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void sendBroadcastAsUser(final Intent intent, final UserHandle user) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void sendBroadcastAsUser(
+            final Intent intent,
+            final UserHandle user,
+            @Nullable final String receiverPermission
+    ) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void sendOrderedBroadcastAsUser(
+            final Intent intent,
+            final UserHandle user,
+            @Nullable final String receiverPermission,
+            final BroadcastReceiver resultReceiver,
+            @Nullable final Handler scheduler,
+            final int initialCode,
+            @Nullable final String initialData,
+            @Nullable final Bundle initialExtras
+    ) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void sendStickyBroadcast(final Intent intent) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void sendStickyOrderedBroadcast(
+            final Intent intent,
+            final BroadcastReceiver resultReceiver,
+            @Nullable final Handler scheduler,
+            final int initialCode,
+            @Nullable final String initialData,
+            @Nullable final Bundle initialExtras
+    ) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void removeStickyBroadcast(final Intent intent) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void sendStickyBroadcastAsUser(final Intent intent, final UserHandle user) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void sendStickyOrderedBroadcastAsUser(
+            final Intent intent,
+            final UserHandle user,
+            final BroadcastReceiver resultReceiver,
+            @Nullable final Handler scheduler,
+            final int initialCode,
+            @Nullable final String initialData,
+            @Nullable final Bundle initialExtras
+    ) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void removeStickyBroadcastAsUser(final Intent intent, final UserHandle user) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Nullable
+    @Override
+    public Intent registerReceiver(@Nullable final BroadcastReceiver receiver, final IntentFilter filter) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Nullable
+    @Override
+    public Intent registerReceiver(
+            @Nullable final BroadcastReceiver receiver,
+            final IntentFilter filter,
+            final int flags
+    ) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Nullable
+    @Override
+    public Intent registerReceiver(
+            final BroadcastReceiver receiver,
+            final IntentFilter filter,
+            @Nullable final String broadcastPermission,
+            @Nullable final Handler scheduler
+    ) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Nullable
+    @Override
+    public Intent registerReceiver(
+            final BroadcastReceiver receiver,
+            final IntentFilter filter,
+            @Nullable final String broadcastPermission,
+            @Nullable final Handler scheduler,
+            final int flags
+    ) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void unregisterReceiver(final BroadcastReceiver receiver) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Nullable
+    @Override
+    public ComponentName startService(final Intent service) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Nullable
+    @Override
+    public ComponentName startForegroundService(final Intent service) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean stopService(final Intent service) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean bindService(final Intent service, @NonNull final ServiceConnection conn, final int flags) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void unbindService(@NonNull final ServiceConnection conn) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean startInstrumentation(
+            @NonNull final ComponentName className,
+            @Nullable final String profileFile,
+            @Nullable final Bundle arguments
+    ) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Object getSystemService(@NonNull final String name) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Nullable
+    @Override
+    public String getSystemServiceName(@NonNull final Class<?> serviceClass) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int checkPermission(@NonNull final String permission, final int pid, final int uid) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int checkCallingPermission(@NonNull final String permission) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int checkCallingOrSelfPermission(@NonNull final String permission) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int checkSelfPermission(@NonNull final String permission) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void enforcePermission(
+            @NonNull final String permission,
+            final int pid,
+            final int uid,
+            @Nullable final String message
+    ) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void enforceCallingPermission(@NonNull final String permission, @Nullable final String message) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void enforceCallingOrSelfPermission(@NonNull final String permission, @Nullable final String message) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void grantUriPermission(final String toPackage, final Uri uri, final int modeFlags) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void revokeUriPermission(final Uri uri, final int modeFlags) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void revokeUriPermission(final String toPackage, final Uri uri, final int modeFlags) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int checkUriPermission(final Uri uri, final int pid, final int uid, final int modeFlags) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int checkCallingUriPermission(final Uri uri, final int modeFlags) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int checkCallingOrSelfUriPermission(final Uri uri, final int modeFlags) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int checkUriPermission(
+            @Nullable final Uri uri,
+            @Nullable final String readPermission,
+            @Nullable final String writePermission,
+            final int pid,
+            final int uid,
+            final int modeFlags
+    ) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void enforceUriPermission(
+            final Uri uri,
+            final int pid,
+            final int uid,
+            final int modeFlags,
+            final String message
+    ) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void enforceCallingUriPermission(final Uri uri, final int modeFlags, final String message) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void enforceCallingOrSelfUriPermission(final Uri uri, final int modeFlags, final String message) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void enforceUriPermission(
+            @Nullable final Uri uri,
+            @Nullable final String readPermission,
+            @Nullable final String writePermission,
+            final int pid,
+            final int uid,
+            final int modeFlags,
+            @Nullable final String message
+    ) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Context createPackageContext(final String packageName, final int flags) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Context createContextForSplit(final String splitName) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Context createConfigurationContext(@NonNull final Configuration overrideConfiguration) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Context createDisplayContext(@NonNull final Display display) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Context createDeviceProtectedStorageContext() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean isDeviceProtectedStorage() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean isRestricted() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Executor getMainExecutor() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void registerComponentCallbacks(final ComponentCallbacks callback) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void unregisterComponentCallbacks(final ComponentCallbacks callback) {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ sharedpreferencesmock/src/test/java/com/github/ivanshafran/sharedpreferencesmock/SPMockContextThrowsTest.kt
@@ -0,0 +1,85 @@
+package com.github.ivanshafran.sharedpreferencesmock
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.util.AttributeSet
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+import java.lang.reflect.InvocationTargetException
+import kotlin.reflect.KFunction
+import kotlin.reflect.KVisibility
+import kotlin.reflect.full.createType
+import kotlin.reflect.full.functions
+import kotlin.test.assertFailsWith
+
+private val INT_TYPE = Int::class.createType()
+private val ANY_FUNCTIONS = listOf(
+        Context::equals,
+        Context::hashCode,
+        Context::toString
+)
+
+// For overload functions
+private fun <Type> getFunctionReference(reference: Type): Type {
+    return reference
+}
+
+private val CONTEXT_FINAL_FUNCTIONS = listOf(
+        Context::getColorStateList,
+        Context::getDrawable,
+        getFunctionReference<Context.(Int) -> String>(Context::getString),
+        getFunctionReference<Context.(Int, Array<Any>) -> String>(Context::getString),
+        Context::getText,
+        Context::getColor,
+        getFunctionReference<Context.(IntArray) -> TypedArray>(Context::obtainStyledAttributes),
+        getFunctionReference<Context.(Int, IntArray) -> TypedArray>(Context::obtainStyledAttributes),
+        getFunctionReference<Context.(AttributeSet, IntArray) -> TypedArray>(Context::obtainStyledAttributes),
+        getFunctionReference<Context.(AttributeSet, IntArray, Int, Int) -> TypedArray>(Context::obtainStyledAttributes),
+        getFunctionReference<Context.(Class<Any>) -> Any>(Context::getSystemService)
+)
+
+class SPMockContextThrowsTest : Spek({
+
+    val contextMock = SPMockContext()
+    val functions = Context::class.functions
+            .filter { it.visibility == KVisibility.PUBLIC }
+            .filter { it !in ANY_FUNCTIONS }
+            .filter { it !in CONTEXT_FINAL_FUNCTIONS }
+
+    fun getCallArgumentMocks(function: KFunction<*>): Array<Any?> {
+        return function.parameters
+                .subList(1, function.parameters.size)
+                .map {
+                    when (it.type) {
+                        INT_TYPE -> 0
+                        else -> null
+                    }
+                }
+                .toTypedArray()
+    }
+
+    fun rethrowInvocationTargetExceptionCause(block: () -> Unit) {
+        try {
+            block()
+        } catch (e: InvocationTargetException) {
+            throw e.cause ?: return
+        }
+    }
+
+    describe("mock context") {
+
+        functions.forEach { function ->
+            context("on $function call") {
+                it("should throw UnsupportedOperationException") {
+                    assertFailsWith<UnsupportedOperationException> {
+                        rethrowInvocationTargetExceptionCause {
+                            val args = getCallArgumentMocks(function)
+                            function.call(contextMock, *args)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+})
