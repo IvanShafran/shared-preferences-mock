6372abe984d97553e6f14ff36dfd6cf29cdbe052
Ivan Shafran <shafran.fun@gmail.com>
2019-06-23 21:07:18 +0300

    Add test for string set processing

---
+++ sharedpreferencesmock/src/test/java/com/github/ivanshafran/sharedpreferencesmock/SharedPreferencesStringSetTest.kt
@@ -0,0 +1,40 @@
+package com.github.ivanshafran.sharedpreferencesmock
+
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
+
+class SharedPreferencesStringSetTest : Spek({
+
+    val key = "key"
+    describe("string set processing in shared preferences") {
+        val sharedPreferences by memoized { SPMockBuilder().createSharedPreferences() }
+
+        context("on null string set") {
+            beforeEachTest {
+                sharedPreferences.edit().putStringSet(key, null).commit()
+            }
+
+            it("should save and return null string set") {
+                assertNull(sharedPreferences.getStringSet(key, null))
+            }
+        }
+
+        context("on nonnull string set") {
+            val stringSet = setOf("1", "2")
+            var returnedSet: Set<String>? = null
+            beforeEachTest {
+                sharedPreferences.edit().putStringSet(key, stringSet).commit()
+                returnedSet = sharedPreferences.getStringSet(key, null)
+            }
+
+            it("should save and return copy of it") {
+                assertEquals(stringSet, returnedSet)
+                assertTrue { returnedSet !== stringSet }
+            }
+        }
+    }
+
+})
