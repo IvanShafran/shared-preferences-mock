b1e44cbb2425402c9c41b40544fb5ac95fb6834d
Ivan Shafran <shafran.fun@gmail.com>
2019-06-23 20:59:20 +0300

    Add test for remove and clear

---
+++ sharedpreferencesmock/src/test/java/com/github/ivanshafran/sharedpreferencesmock/SharedPreferencesDeleteTest.kt
@@ -0,0 +1,37 @@
+package com.github.ivanshafran.sharedpreferencesmock
+
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class SharedPreferencesDeleteTest : Spek({
+
+    describe("deleting elements from shared preferences mock") {
+        val sharedPreferences by memoized { SPMockBuilder().createSharedPreferences() }
+
+        context("on calling clear in edit builder") {
+            beforeEachTest {
+                sharedPreferences.edit().putInt("1", 1).putBoolean("2", false).commit()
+                sharedPreferences.edit().clear().commit()
+            }
+
+            it("should delete all items") {
+                assertTrue(sharedPreferences.all.isEmpty())
+            }
+        }
+
+        context("on calling remove in edit builder") {
+            beforeEachTest {
+                sharedPreferences.edit().putInt("1", 1).putBoolean("2", false).commit()
+                sharedPreferences.edit().remove("1").commit()
+            }
+
+            it("should remove only specific item") {
+                assertFalse(sharedPreferences.contains("1"))
+                assertTrue(sharedPreferences.contains("2"))
+            }
+        }
+    }
+
+})
