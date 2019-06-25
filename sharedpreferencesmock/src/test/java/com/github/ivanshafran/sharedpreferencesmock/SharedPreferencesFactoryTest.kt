0a545019ab9912c3958fdddb161896a2bfe0f10d
Ivan Shafran <shafran.fun@gmail.com>
2019-06-25 23:41:04 +0300

    Add test for factory

---
+++ sharedpreferencesmock/src/test/java/com/github/ivanshafran/sharedpreferencesmock/SharedPreferencesFactoryTest.kt
@@ -0,0 +1,34 @@
+package com.github.ivanshafran.sharedpreferencesmock
+
+import android.content.SharedPreferences
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+import kotlin.test.assertTrue
+
+class SharedPreferencesFactoryTest : Spek({
+
+    describe("shared preferences factory") {
+        lateinit var preferences: SharedPreferences
+
+        context("thread safe flag is enabled") {
+            beforeEachTest {
+                preferences = SharedPreferencesFactory(true).create()
+            }
+
+            it("should create thread safe preferences instance") {
+                assertTrue { preferences is ThreadSafeSharedPreferencesMock }
+            }
+        }
+
+        context("thread safe flag is disabled") {
+            beforeEachTest {
+                preferences = SharedPreferencesFactory(false).create()
+            }
+
+            it("should create not thread safe preferences instance") {
+                assertTrue { preferences is SharedPreferencesMock }
+            }
+        }
+    }
+
+})
